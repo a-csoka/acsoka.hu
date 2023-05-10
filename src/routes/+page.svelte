c2957819faface52ece26b6624d6934e12424a5c
ACsóka <acsokacsaba@gmail.com>
2023-05-10 21:25:07 +0200

    Created Svelte App

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Sidebar from './Components/Sidebar.svelte';
+</script>
+
+<div class="h-screen w-screen">
+    <Sidebar />
+</div>
---
+++ src/routes/Components/Sidebar.svelte
@@ -0,0 +1,3 @@
+<div class="fixed hidden bg-black h-[90%] w-[96px] left-[1%] top-[5%] rounded-xl lg:block">
+teszt
+</div>
