63cff1cf744f1a7b685261afd9d65adbae16f894
Csóka Csaba <acsokacsaba@gmail.com>
2023-06-11 01:12:14 +0200

    Some design

--- src/routes/Components/Contact.svelte
+++ src/routes/Components/Contact.svelte
@@ -5,7 +5,7 @@
 <section class="pt-8 bg-zinc-900 ">
     <div class="text-white text-center font-Gilmer text-4xl pt-8">Elérhetőség</div>
 
-    <div id="Contact" class="grid grid-cols-1 md:grid-cols-4 py-16 mx-auto">
+    <div id="Contact" class="grid grid-cols-1 md:grid-cols-4 py-16 mx-auto xl:mx-60 2xl:mx-96">
         <ContactComponent Image="Images/Contact/Facebook.png" Link="https://www.facebook.com/csoka.csaba2003"/>
         <ContactComponent Image="Images/Contact/Instagram.png" Link="https://www.instagram.com/acsoka_10/"/>
         <ContactComponent Image="Images/Contact/Discord.png" Link="https://discord.com/users/1117117676238688387"/>
