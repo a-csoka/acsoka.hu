1d4be0e3d697ed0f7d8f6aaf5b329d8e56dca4c0
Csóka Csaba <acsokacsaba@gmail.com>
2023-06-11 02:05:01 +0200

    Fixed image clipping

--- src/routes/Components/Education.svelte
+++ src/routes/Components/Education.svelte
@@ -2,7 +2,7 @@
     import EducationCard from "./EducationCard.svelte";
 </script>
 
-<section id="Education" class="w-full h-fit bg-gradient-to-b from-black from-0% to-zinc-900">
+<section id="Education" class="w-full h-fit bg-gradient-to-b from-black from-0% to-zinc-900 mt-[-5px]">
     <div class="text-white text-center font-Gilmer text-4xl pt-8">Tanulmányok</div>
     <div class="w-full py-8 grid grid-cols-1 md:grid-cols-2">
         <EducationCard Image="Images/Education/NTSZKI.png" Name="NTSZKI" Title="Számítógépes rendszerkarbantartó" Time="2022. 06. 16." Color="bg-[#20376B]"/>
