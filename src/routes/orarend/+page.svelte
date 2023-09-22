334ed120407ca40f2f36b86a37e80ca8ca7e969f
Csóka Csaba <acsokacsaba@gmail.com>
2023-09-22 23:46:35 +0200

    Added orarend subpage
    Added Orarend, might reduce the spaghetti code later.

---
+++ src/routes/orarend/+page.svelte
@@ -0,0 +1,81 @@
+<script>
+    import { onMount } from "svelte";
+
+    const Days = ["Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek"]
+    let currentDay = (new Date()).getDay()
+    if (currentDay < 1 || currentDay > 5) {
+        currentDay = 1
+    }
+
+    let classes = [
+        {
+            Name: "Matematikai alapok",
+            Classroom: "Déli tömb 0-817(Dudich Endre terem)",
+            Type: "Gyakorlat",
+            Day: 1,
+            StartTime: 8,
+            EndTime: 10,
+            Length: 2,
+            Height: "6rem"
+        },
+        {
+            Name: "TEszt",
+            Classroom: "Déli tömb 0-817(Dudich Endre terem)",
+            Type: "Gyakorlat",
+            Day: 2,
+            StartTime: 12,
+            EndTime: 14,
+            Length: 2,
+            Height: "6rem"
+        }
+    ]
+
+    const fetchClasses = async () => {
+        let response = await (fetch("https://www.acsoka.hu/GetOrarend.php", {
+            method: "GET",
+            credentials: 'omit',
+        }))
+        response = await response.json()
+        return response
+    }
+    
+    onMount(async () => {
+        classes = await fetchClasses()
+
+        classes.map((element, index) => {
+            classes[index].Height = ((element.EndTime-element.StartTime)/60*6)+"rem"
+        })
+    })
+</script>
+
+<div class="fixed flex flex-row w-full h-full overflow-y-auto overflow-x-hidden">
+    <div class="relative w-16 mt-[4.5rem] h-full text-center">
+        {#each {length: 15} as _, hour}
+            <div class="w-16 h-[6rem] text-white">
+                <div class="w-screen h-[0.1rem] bg-gray-200"/>
+                <div class="font-Brooklyn text-gray-200">{(hour+8 < 10 ? "0"+(hour+8) : hour+8)}:00</div>
+            </div>
+        {/each}
+    </div>
+    <div class="relative flex-col w-full h-full">
+        <div class="relative flex flex-row h-16 text-gray-200 rounded-t-xl mt-2 font-Gilmer">
+            {#each Days as DayName, Index}
+                    <button class="w-full h-full border border-black rounded-t-xl {(Index + 1 == currentDay ? "bg-[#00c5f0] text-black" : "bg-zinc-800")}" on:click={() => currentDay = Index+1}>{DayName}</button>
+            {/each}
+        </div>
+        <div class="w-full h-full">
+            {#each classes as element, Index}
+                <div class="absolute hidden md:block w-[20%] rounded-xl bg-[#009db4] border border-[#006b82] " style="height: {element.Height}; left: {(element.Day-1)*20}%; top: {(element.StartTime-480)/60*6+4.5}rem;">
+                    <div class="text-center font-Gilmer text-lg font-bold mt-2">{element.Name}</div>
+                    <div class="text-center font-Gilmer">{element.Classroom}</div>
+                    <div class="text-center font-Gilmer">{element.TimeText}</div>
+                </div>
+                <div class="absolute {(element.Day == currentDay ? "block" : "hidden")} md:hidden w-[100%] rounded-xl bg-[#009db4] border border-[#006b82] " style="height: {element.Height}; top: {(element.StartTime-480)/60*6+4.5}rem;">
+                    <div class="text-center font-Gilmer text-lg font-bold mt-2">{element.Name}</div>
+                    <div class="text-center font-Gilmer">{element.Classroom}</div>
+                    <div class="text-center font-Gilmer">{element.TimeText}</div>
+                </div>
+            {/each}
+        </div>
+    </div>
+</div>
