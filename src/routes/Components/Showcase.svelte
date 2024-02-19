72afdf8a4361bd653d854ec6821d57e6f8a7a577
Csóka Csaba <acsokacsaba@gmail.com>
2024-02-19 22:12:49 +0100

    Added shortcut to Showcase
    - Now the visitors can use the shortcut buttons to see which item they are seeing and click on them to change current item.
    - If the user clicks on an item which is not the next in line the animation will be left to right. Otherwise it's right to left

--- src/routes/Components/Showcase.svelte
+++ src/routes/Components/Showcase.svelte
@@ -1,22 +1,52 @@
 <script>
     import { onMount } from 'svelte';
-    import { slide, fly } from 'svelte/transition';
     import ShowcaseCard from './ShowcaseCard.svelte';
+
+    let timer
 
     let directions = [0, 100]
     let workCount = 0
+    let nextCont = 0
+    let isMoving = false
+    let direction = "LeftToRight"
     function showcaseMover(){
-        setTimeout(() => {
+        timer = setTimeout(() => {
+            forceMove((workCount+1 < works.length-1 ? workCount+1 : 0))
+        }, 15000)
+    }
+
+    function forceMove(next){
+        nextCont = next
+        clearTimeout(timer)
+        
+        if(next > workCount || (next == 0 && workCount == works.length-1)){
+            directions = [0, 100]
+            direction = "LeftToRight"
+        }else{
             directions = [-100, 0]
-            setTimeout(() => {
-                workCount = workCount + 1
-                if(workCount > works.length-1){
-                    workCount = 0
+            direction = "RightToLeft"
+        }
+
+        timer = setTimeout(() => {
+            if(direction == "RightToLeft"){
+                isMoving = true
+                directions = [0, 100]
+            }else{
+                isMoving = true
+                directions = [-100, 0]
+            }
+            timer = setTimeout(() => {
+                direction = "LeftToRight"
+                workCount = next
+                nextCont = workCount + 1
+                if(nextCont > works.length-1){
+                    nextCont = 0
                 }
+                isMoving = false
                 directions = [0, 100]
             }, 1000)
             showcaseMover()
-        }, 15000)
+        }, 50)
     }
     let ready = false;
     onMount(() => {
@@ -25,18 +55,18 @@
     });
     const works = [
         {
-            Name: "Magyar Hot Pursuit",
-            NameLink: "https://discord.gg/x3FTPXn8KU",
-            Title: "Fejlesztő",
-            Description: "Egy Multi Theft Auto: San Andreas szerver, amely Lua-ban írodott és MySQL adatbázist használ. Jelenleg két féle játékmód található meg benne. Hot Pursuit, amelyben a játékosok egy vagy két személyes csapatokban üldöznek egy kiválaszott csapatot. Clan War, itt pedig két csapat küzd a pályán található pontok elfoglalásáért. A fejlesztésése során a szerver alapjáért, a kezdetleges kezelőfelületekért, valamint kisebb rendszerekért feleltem, mint például: Emeltdíjas hívások automatikus kezelése, Discord bot és webhook, járművek fájlainak titkosítása.",
-            Image: "Images/Showcase/mhp.png"
-        },
-        {
             Name: "DBLock",
             NameLink: "https://www.acsoka.hu/dblock/",
             Title: "Készítő",
             Description: "A weboldalt egy öt fős csoportban kellett elkészítenünk egy megadott példa szerint. Az oldalt egy olasz cégnek csináltuk az Erasmus+ keretein belül, viszont később egyedül is megcsináltam körülbelül 18 óra alatt. A két felhasznált technológia a Svelte és a Bootstrap.",
             Image: "Images/Showcase/dblock.png",
+        },
+        {
+            Name: "Magyar Hot Pursuit",
+            NameLink: "https://discord.gg/x3FTPXn8KU",
+            Title: "Fejlesztő",
+            Description: "Egy Multi Theft Auto: San Andreas szerver, amely Lua-ban írodott és MySQL adatbázist használ. Jelenleg két féle játékmód található meg benne. Hot Pursuit, amelyben a játékosok egy vagy két személyes csapatokban üldöznek egy kiválaszott csapatot. Clan War, itt pedig két csapat küzd a pályán található pontok elfoglalásáért. A fejlesztésése során a szerver alapjáért, a kezdetleges kezelőfelületekért, valamint kisebb rendszerekért feleltem, mint például: Emeltdíjas hívások automatikus kezelése, Discord bot és webhook, járművek fájlainak titkosítása.",
+            Image: "Images/Showcase/mhp.png"
         },
         {
             Name: "ReserveIt",
@@ -51,11 +81,11 @@
 
 <section class="relative flex flex-row w-full min-h-fit overflow-x-clip">
     {#if ready}
-        <div class="relative table-cell min-w-full h-full color-white text-gray-200 items-center" style="left: {directions[0]}%; transition: left {(directions[0] == -100 ? 1 : 0)}s">
-            <ShowcaseCard Name={works[workCount].Name} NameLink={works[workCount].NameLink} Title={works[workCount].Title} Description={works[workCount].Description} Image={works[workCount].Image}/>
+        <div class="relative table-cell min-w-full h-full color-white text-gray-200 items-center" style="left: {directions[0]}%; transition: left {(isMoving ? 1 : 0)}s">
+            <ShowcaseCard Name={works[(direction == "LeftToRight" ? workCount : nextCont)].Name} NameLink={works[(direction == "LeftToRight" ? workCount : nextCont)].NameLink} Title={works[(direction == "LeftToRight" ? workCount : nextCont)].Title} Description={works[(direction == "LeftToRight" ? workCount : nextCont)].Description} Image={works[(direction == "LeftToRight" ? workCount : nextCont)].Image} Current={(direction == "LeftToRight" ? workCount : nextCont)} Limit={works.length} forceMove={forceMove}/>
         </div>
-        <div class="absolute min-w-full h-full color-white text-gray-200 items-center" style="left: {directions[1]}%; transition: left {(directions[1] == 0 ? 1 : 0)}s">
-            <ShowcaseCard Name={works[(workCount >= works.length-1 ? 0 : workCount+1)].Name} NameLink={works[(workCount >= works.length-1 ? 0 : workCount+1)].NameLink} Title={works[(workCount >= works.length-1 ? 0 : workCount+1)].Title} Description={works[(workCount >= works.length-1 ? 0 : workCount+1)].Description} Image={works[(workCount >= works.length-1 ? 0 : workCount+1)].Image}/>
+        <div class="absolute min-w-full h-full color-white text-gray-200 items-center" style="left: {directions[1]}%; transition: left {(isMoving ? 1 : 0)}s">
+            <ShowcaseCard Name={works[(direction == "LeftToRight" ? nextCont : workCount)].Name} NameLink={works[(direction == "LeftToRight" ? nextCont : workCount)].NameLink} Title={works[(direction == "LeftToRight" ? nextCont : workCount)].Title} Description={works[(direction == "LeftToRight" ? nextCont : workCount)].Description} Image={works[(direction == "LeftToRight" ? nextCont : workCount)].Image} Current={(direction == "LeftToRight" ? nextCont : workCount)} Limit={works.length} forceMove={forceMove}/>
         </div>
     {/if}
 </section>
--- src/routes/Components/ShowcaseCard.svelte
+++ src/routes/Components/ShowcaseCard.svelte
@@ -1,19 +1,31 @@
 <script>
+	import { each } from "svelte/internal";
+
     export let Name;
     export let NameLink;
     export let Title;
     export let Description;
     export let Image;
+
+    export let Current;
+    export let Limit;
+
+    export let forceMove;
 </script>
 
 <div class="w-full h-fit overflow-y-visible">
     <img class="relative md:absolute w-full h-160 object-cover" src={Image} alt="Hiba a betöltés során!">
     <div class="relative w-full h-[40rem] md:h-160 grid grid-cols-2 md:top-0">
-        <div class="bg-black bg-opacity-100 md:bg-opacity-90 col-span-2 md:col-span-1 md:col-start-2">
+        <div class="grid bg-black bg-opacity-100 md:bg-opacity-90 col-span-2 md:col-span-1 md:col-start-2 content-between">
             <div class="my-3 mx-5 text-center md:mt-20 h-fit">
                 <a class="font-Brooklyn text-3xl" href={NameLink}>{Name}</a>
                 <div class="font-Gilmer text-xl mb-10">{Title}</div>
                 <div class="font-Gilmer md:text-lg">{Description}</div>
+            </div>
+            <div class="flex w-full h-5 self-end mb-10 justify-center content-center">
+                {#each {length: Limit} as _, i}
+                    <button class="rounded-full border-2 border-gray-200 aspect-square h-full mx-0.5 {Current == i ? "bg-gray-200" : ""}" on:click={() => {forceMove(i)}}></button>
+                {/each}
             </div>
         </div>
     </div>
