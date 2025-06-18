2801b26bb1ed6aa5e832c5937f419d43dfe704ce
Csóka Csaba <acsokacsaba@gmail.com>
2025-06-18 18:51:34 +0200

    add contact information to AboutMe section

---
+++ app/assets/data/contacts.ts
@@ -0,0 +1,27 @@
+import FacebookIcon from "../images/icons/Facebook_icon.svg"
+import DiscordIcon from "../images/icons/Discord_icon.svg"
+import GitHubIcon from "../images/icons/GitHub_icon.svg"
+import InstagramIcon from "../images/icons/Instagram_icon.svg"
+
+export const ContactList = [
+    {
+        Name: "Facebook",
+        Image: FacebookIcon,
+        Link: "https://www.facebook.com/csoka.csaba2003",
+    },
+    {
+        Name: "Instagram",
+        Image: InstagramIcon,
+        Link: "https://www.instagram.com/_acsoka_/",
+    },
+    {
+        Name: "Discord",
+        Image: DiscordIcon,
+        Link: "https://discordapp.com/users/1117117676238688387",
+    },
+    {
+        Name: "GitHub",
+        Image: GitHubIcon,
+        Link: "https://github.com/a-csoka",
+    },
+]
--- app/components/AboutMe_component.tsx
+++ app/components/AboutMe_component.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { useLanguage } from '~/context/LanguageContext';
 
+import { ContactList } from '~/assets/data/contacts';
+
 import Me from "../assets/images/Me.jpg"
 import SectionTitle from './SectionTitle_component';
+import { Link } from 'react-router';
 
 const AboutMe: React.FC = () => {
     const LangContext = useLanguage()
@@ -11,7 +14,7 @@ const AboutMe: React.FC = () => {
         <div className='w-full'>
             <SectionTitle>{LangContext.translations.AboutMe["header"]}</SectionTitle>
             <div className='grid grid-cols-2 w-full h-full'>
-                <div className='flex flex-col justify-end items-center my-12'>
+                <div className='flex flex-col justify-end items-center my-12 mx-30'>
                     <div>
                         <div className='flex w-100 h-100 rounded-full bg-gradient-to-r from-MainBlue to-LuckyGreen items-center justify-center'>
                                 <img
@@ -22,6 +25,13 @@ const AboutMe: React.FC = () => {
                         </div>
                         <div className='text-white text-center mt-4 text-4xl font-Montserrat text-shadow-lg text-shadow-black'>{LangContext.translations.AboutMe["name"]}</div>
                         <div className='text-white text-center mt-1 text-md font-Montserrat text-shadow-lg text-shadow-black'>{LangContext.translations.AboutMe["title"]}</div>
+                    </div>
+                    <div className='grid grid-cols-4 w-full mt-10'>
+                        {ContactList.map((contact, index) => (
+                            <Link to={contact.Link} key={index} target="_blank" className='flex flex-row justify-center'>
+                                <img className='h-20 aspect-square' src={contact.Image} alt={contact.Name}/>
+                            </Link>
+                        ))}
                     </div>
                 </div>
                 <div className='flex items-center font-Montserrat text-lg text-shadow-lg text-shadow-black'>
